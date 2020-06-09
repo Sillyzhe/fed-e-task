a200190cc12910ae3e39b8a1c43ff5a9a993f350
吴利哲 <2593008451@qq.com>
2020-06-09 08:56:29 +0800

    gulp-flow

--- part03/code/auto-build/gulp-flow/lib/index.js
+++ part03/code/auto-build/gulp-flow/lib/index.js
@@ -9,126 +9,135 @@ const {
 const del = require('del')
 const browserSync = require('browser-sync')
 
+// cwd返回当前命令行的工作目录
+const cwd = process.cwd()
+let config = {
+  // default config
+  build: {
+    src: 'src',
+    dist: 'dist',
+    temp: 'temp',
+    public: 'public',
+    paths: {
+      styles: 'assets/styles/*.scss',
+      scripts: 'assets/scripts/*.js',
+      pages: '*.html',
+      images: 'assets/images/**',
+      fonts: 'assets/fonts/**'
+    }
+  }
+}
 
+try {
+  const loadConfig = require(`${cwd}/pages.config.js`)
+  config = Object.assign({}, assign, loadConfig)
+} catch (e) {}
 
 const loadPlugins = require('gulp-load-plugins');
 const plugins = loadPlugins()
 
 const bs = browserSync.create()
 
-const data = {
-  menus: [{
-      name: 'Home',
-      icon: 'aperture',
-      link: 'index.html'
-    },
-    {
-      name: 'Features',
-      link: 'features.html'
-    },
-    {
-      name: 'About',
-      link: 'about.html'
-    },
-    {
-      name: 'Contact',
-      link: '#',
-      children: [{
-          name: 'Twitter',
-          link: 'https://twitter.com/w_zce'
-        },
-        {
-          name: 'About',
-          link: 'https://weibo.com/zceme'
-        },
-        {
-          name: 'divider'
-        },
-        {
-          name: 'About',
-          link: 'https://github.com/zce'
-        }
-      ]
-    }
-  ],
-  pkg: require('./package.json'),
-  date: new Date()
-}
 
 const clean = () => {
-  return del(['dist', 'temp'])
+  return del([config.build.dist, config.build.temp])
 }
 
 const style = () => {
-  return src('src/assets/styles/*.scss', {
-      base: 'src'
+  return src(config.build.paths.styles, {
+      base: config.build.src,
+      cwd: config.build.src
     })
     .pipe(plugins.sass({
       outputStyle: 'expanded'
     }))
-    .pipe(dest('temp'))
+    .pipe(dest(config.build.temp))
     .pipe(bs.reload({
       stream: true
     }))
 }
 
 const script = () => {
-  return src('src/assets/scripts/*.js', {
-      base: 'src'
+  return src(config.build.paths.scripts, {
+      base: config.build.src,
+      cwd: config.build.src
     })
     .pipe(plugins.babel({
-      presets: ['@babel/preset-env']
+      presets: [require('@babel/preset-env')]
     }))
-    .pipe(dest('temp'))
+    .pipe(dest(config.build.temp))
     .pipe(bs.reload({
       stream: true
     }))
 }
 
 const page = () => {
-  return src('src/*.html', {
-      base: 'src'
+  return src(config.build.paths.pages, {
+      base: config.build.src,
+      cwd: config.build.src
     })
     .pipe(plugins.swig({
-      data
+      data: config.data
     }))
-    .pipe(dest('temp'))
+    .pipe(dest(config.build.temp))
     .pipe(bs.reload({
       stream: true
     }))
 }
 
 const image = () => {
-  return src('src/assets/images/**', {
-      base: 'src'
+  return src(config.build.paths.images, {
+      base: config.build.src,
+      cwd: config.build.src
     })
     .pipe(plugins.imagemin())
-    .pipe(dest('dist'))
+    .pipe(dest(config.build.dist))
 }
 
 const font = () => {
-  return src('src/assets/fonts/**', {
-      base: 'src'
+  return src(config.build.paths.fonts, {
+      base: config.build.src,
+      cwd: config.build.src
     })
     .pipe(plugins.imagemin())
-    .pipe(dest('dist'))
+    .pipe(dest(config.build.dist))
 }
 
 const extra = () => {
-  return src('public/**', {
-      base: 'public'
+  return src('**', {
+      base: config.build.public,
+      cwd: config.build.public
     })
-    .pipe(dest('dist'))
+    .pipe(dest(config.build.dist))
 }
 
 const serve = () => {
-  watch('src/assets/styles/*.scss', style)
-  watch('src/assets/scripts/*.js', script)
-  watch('src/*.html', page)
+  watch(config.build.paths.styles, {
+    cwd: config.build.src
+  }, style)
+  watch(config.build.paths.scripts, {
+    cwd: config.build.src
+  }, script)
+  watch(config.build.paths.pages, {
+    cwd: config.build.src
+  }, page)
   // watch('src/assets/images/**', image)
   // watch('src/assets/fonts/**', font)
   // watch('public/**', extra)
-  watch(['src/assets/images/**', 'src/assets/fonts/**', 'public/**'], bs.reload)
+  watch([config.build.paths.images, {
+        cwd: config.build.src
+      },
+      config.build.paths.scripts, {
+        cwd: config.build.src
+      },
+    ], {
+      cwd: config.build.src
+    }, bs
+    .reload)
+
+  watch('**', {
+    cwd: config.build.public
+  }, bs.reload)
 
   bs.init({
     port: 2080,
@@ -136,7 +145,7 @@ const serve = () => {
     // open:false,
     // files: 'dist/**',
     server: {
-      baseDir: ['temp', 'src', 'public'],
+      baseDir: [config.build.temp, config.build.src, config.build.public],
       routes: {
         '/node_modules': 'node_modules'
       }
@@ -145,11 +154,12 @@ const serve = () => {
 }
 
 const useref = () => {
-  return src('temp/*.html', {
-      base: 'temp'
+  return src(config.build.paths.pages, {
+      base: config.build.temp,
+      cwd: config.build.src
     })
     .pipe(plugins.useref({
-      searchPath: ['temp', '.']
+      searchPath: [config.build.temp, '.']
     }))
     // html js css
     .pipe(plugins.if(/\.js$/, plugins.uglify()))
@@ -159,13 +169,14 @@ const useref = () => {
       minifyCSS: true,
       minifyJS: true
     })))
-    .pipe(dest('dist'))
+    .pipe(dest(config.build.dist))
 }
 
 const compile = parallel(style, script, page)
 
 // 上线前执行的任务
-const build = series(clean, parallel(series(compile, useref), image, font, extra))
+const build = series(clean, parallel(series(compile, useref), image, font,
+  extra))
 
 // 开发时执行的任务
 const develop = series(compile, serve)
